390c0cf0b8a917ce6524006130640b15e3b9d657
Satenik15 <hakhverdyansatenik@gmail.com>
2024-07-05 12:54:15 +0400

    remove animations

--- src/components/about/about.component.ts
+++ src/components/about/about.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { fadeInAnimation } from 'src/animations/fade-in.animation';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css',
-  animations: [fadeInAnimation]
+  styleUrl: './about.component.css'
 })
 export class AboutComponent {
 
--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { slideInFromLeftAnimation } from 'src/animations/slide-in-from-left.animation';
-import { slideInFromRightAnimation } from 'src/animations/slide-in-from-right.animation';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
-  animations: [slideInFromRightAnimation,slideInFromLeftAnimation]
+  styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
